767dd49e51d78812339e02dda75f04c42b46629c
Jorgen-Jr <jorgenjr@outlook.com>
2021-04-07 17:12:44 -0300

    fix: Header was breaking line where it shouldn't.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -114,7 +114,7 @@ const Header = ({ slide }: IProps) => {
           fontSize={["unset", "unset", "0.7rem", "unset"]}
           as="nav"
         >
-          <Flex alignContent="center" justifyContent="center" flexDir="column">
+          <Flex alignContent="center" justifyContent="center" flexDir="column" w="100%">
             <Text fontFamily="FiraCode" fontSize="xl">
               Jorge.room{" "}
               <MotionText
